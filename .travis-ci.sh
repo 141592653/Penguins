0bf0150d92b232db0fb2c9754e05c7e05331cf13
Alice Rixte <alice.rixte@gmail.com>
2017-09-29 18:02:34 +0200

    travis2

--- .travis-ci.sh
+++ .travis-ci.sh
@@ -1,5 +1,5 @@
 # Edit this for your own project dependencies
-OPAM_DEPENDS="ocamlfind ounit re"
+OPAM_DEPENDS="ocamlfind ounit lablgtk ocamlbuild"
 	 
 case "$OCAML_VERSION,$OPAM_VERSION" in
 4.04.0,1.2.2) ppa=avsm/ocaml44+opam12 ;;
@@ -10,8 +10,10 @@ echo "yes" | sudo add-apt-repository ppa:$ppa
 sudo apt-get update -qq
 sudo apt-get install -qq ocaml ocaml-native-compilers camlp4-extra opam
 export OPAMYES=1
-opam init 
+opam init
+opam switch 4.04.0
 opam install ${OPAM_DEPENDS}
+
 eval `opam config env`
 make
 #make test
