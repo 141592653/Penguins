01372a812fe1d411e56a3a8bd46bf9c1660ec329
pjmkrpg <pjmkrpg@users.noreply.github.com>
2017-10-16 17:13:15 +0200

    main window widgets in correct order, plus scrollbar

--- src/penguins.ml
+++ src/penguins.ml
@@ -1,27 +1,28 @@
 open GMain
 open GdkKeysyms
 
+(* warning 32 here *)
 let locale = GtkMain.Main.init ()
 
-(* charge un plateau de jeu donné dans Parser.CreateMap *)
-let new_board () =
-  (* ... *)
-  ()
+(** handle quit signal, ask to save game if necessary *)
+let quit () =
+  prerr_endline "Goodbye!";
+  (* TODO *)
+  Main.quit()
 
 (** This is the main function*)
-let main () =
+let [@warning "-48"] main () =
+  (* warning -48 because we like implicit elimination of optional arguments *)
   let window = GWindow.window ~width:320 ~height:240
                               ~title:"Jeu des pingouins" () in
+  ignore (window#connect#destroy ~callback:quit);
+
   let vbox = GPack.vbox ~packing:window#add () in
-  ignore (window#connect#destroy ~callback:Main.quit);
 
   (* Status bar *)
-  (* TODO en bas de la fenêtre, taille fixe *)
-  let status = GMisc.statusbar ~height:1 (* ~width:320 *)
-                               ~packing:vbox#add
-                               () in
-  let st = status#new_context "contexte" in
-  ignore (st#push "bienvenue dans notre jeu");
+  let status = GMisc.statusbar ~packing:(vbox#pack ~from:`END) () in
+  let st = status#new_context ~name:"st" in
+  ignore (st#push "Bienvenue dans notre jeu !");
 
   (* Menu bar *)
   let menubar = GMenu.menu_bar ~packing:vbox#pack () in
@@ -29,44 +30,37 @@ let main () =
   let accel_group = factory#accel_group in
   let file_menu = factory#add_submenu "Fichier" in
 
-
   let load_board () =
     st#pop(); ignore (st#push "Chargement d'un nouveau jeu...");
     (* TODO boîte de dialogue, choisir un fichier de map *)
-    new_board();
     st#pop(); ignore (st#flash ~delay:3000 "Chargement terminé.")
   in
 
   (* File menu *)
   let factory = new GMenu.factory file_menu ~accel_group in
-  ignore (factory#add_item "Nouveau jeu" ~key:_N ~callback: load_board);
-  ignore (factory#add_item "Quitter" ~key:_Q ~callback: Main.quit);
+  ignore (factory#add_item "Nouveau jeu" ~key:_N ~callback:load_board);
+  ignore (factory#add_item "Quitter" ~key:_Q ~callback:quit);
 
-  (* Drawing area. *)
-  let da = GMisc.drawing_area ~packing:vbox#add () in
-  let drawable = lazy (new GDraw.drawable da#misc#window) in
+  (* automatic scrolling bars *)
+  let scroll = GBin.scrolled_window
+                 ~hpolicy:`AUTOMATIC ~vpolicy:`AUTOMATIC
+                 ~packing:vbox#add () in
 
-  let board_size = 35 in
-  let page_size = 3 in
-  (* Create the scrollbar. *)
-  let adjustment =
-    GData.adjustment
-      ~lower:0. ~upper:(float_of_int (board_size-1))
-      ~step_incr:1. ~page_incr:(float_of_int page_size) () in
-  let scrollbar =
-    GRange.scrollbar `HORIZONTAL ~adjustment ~packing:vbox#pack () in
+  (* Drawing area *)
+  let da = GMisc.drawing_area ~packing:scroll#add_with_viewport () in
+  (* TODO taille selon taille de la map chargée *)
+  da#set_size ~height:400 ~width:400;
 
-  (* (\* Button *\) *)
-  (* let button = GButton.button ~label:"1,1" *)
-  (*                             ~packing:vbox#add () in *)
-  (* button#connect#clicked ~callback: (fun () -> prerr_endline "Ouch!"); *)
+  (* let back = GDraw.pixmap ~width:400 ~height:400 () in *)
+  da#misc#realize();                   (* avoid exception Gpointer.Null *)
 
-  (* TODO cast pour que ça compile *)
-  (* vbox#pack status#as_widget; *)
+  let drawable = new GDraw.drawable da#misc#window in
+
 
   (* Display the windows and enter Gtk+ main loop *)
   window#add_accel_group accel_group;
   window#show ();
   Main.main ()
 
-let () = main ()
+let () = Printexc.print main ()
+(* let () = main () *)
--- src/penguins.mli
+++ src/penguins.mli
@@ -1,4 +1,1 @@
 (**This is the main module *)
-
-(** Initialise un nouveau plateau avec les données de la map *)
-(* val new_board : Parser.map -> unit *)
